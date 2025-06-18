cc53b9ef37143210bc86a58ef9cb32d991e273c4
Iskander krayem <iskandermac@Mac.local>
2025-06-18 15:52:39 +0300

    editsss

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-frontend-domain.com', 'https://your-admin-domain.com']
-    : ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:4173'],
+    ? ['https://anaaltabeeb.netlify.app', 'https://your-admin-domain.com']
+    : [ 'http://localhost:5173', 'http://localhost:4173'],
   credentials: true
 }));
 
