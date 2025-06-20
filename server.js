33ad26e3e29706e96bbb07edf4988922fe41622e
Iskander krayem <iskandermac@Mac.local>
2025-06-20 15:27:59 +0300

    testinnggggg

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://anaaltabeeb.netlify.app', 'https://unrivaled-caramel-318cf0.netlify.app']
+    ? ['https://anaaltabeeb.netlify.app', 'https://unrivaled-caramel-318cf0.netlify.app', 'https://unrivaled-caramel-318cf0.netlify.app/login']
     : [ 'http://localhost:5173', 'http://localhost:4173'],
   credentials: true
 }));
